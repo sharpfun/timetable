3a1b8eb93b4d296419e0ed92dc66d8df09b4bb9d
Hans Adler <johannes.aquila@gmail.com>
2015-02-09 00:22:33 +0100

    Created.

--- src/main/java/app/config/DbConfig.java
+++ src/main/java/app/config/DbConfig.java
@@ -5,9 +5,10 @@ import org.javalite.activeweb.AppContext;
 
 public class DbConfig extends AbstractDBConfig {
 
+    @Override
     public void init(AppContext context) {
 
-        environment("development").jdbc("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/db_name_development", "root", "");
+        environment("development").jdbc("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/pokus", "root", "cognitivesystems");
         
         environment("development").testing().jdbc("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/db_name_test", "root", "");
 
